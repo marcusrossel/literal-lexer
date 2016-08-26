0cb0913acd39bb93e87f8029beea2637ea135773
Marcus Rossel <marcusrossel@icloud.com>
2016-08-26 14:56:04 +0200

    Initial Commit

---
+++ Literal Lexer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Literal Lexer
+//
+//  Created by Marcus Rossel on 26.08.16.
+//  Copyright © 2016 Marcus Rossel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
